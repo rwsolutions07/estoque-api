f2f6f601b04ef004384b2e4040b99e2f3a51e014
rwsolutions07 <rwsolutions07@gmail.com>
2022-12-01 18:07:41 -0300

    Primeiro commit

---
+++ src/main/java/rw/solutions/estoque/api/EstoqueApiApplication.java
@@ -0,0 +1,13 @@
+package rw.solutions.estoque.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueApiApplication.class, args);
+	}
+
+}
